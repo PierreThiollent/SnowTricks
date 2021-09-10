a15aac804b55a63a9698e97b077c068d66d769f3
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-09-10 09:54:01 +0200

    Initialize Alpine JS

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,13 @@
 /*
  * Welcome to your app's main JavaScript file!
  *
- * We recommend including the built version of this JavaScript file
+ * We recommend including the built version of profileMenu JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
+// any CSS you import will output into a single css file (app.css in profileMenu case)
 import './styles/app.css';
+import Alpine from 'alpinejs'
 
+window.Alpine = Alpine
+Alpine.start()
