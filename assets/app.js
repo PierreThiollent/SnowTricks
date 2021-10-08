9f4d4b31e725854683b6df177ea3c4da04c8e468
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-10-08 10:56:36 +0200

    Add logic for the ajax load more posts feature.

--- assets/app.js
+++ assets/app.js
@@ -9,5 +9,21 @@
 import './styles/app.css';
 import Alpine from 'alpinejs'
 
-window.Alpine = Alpine
+Alpine.data('loadMorePosts', () => ({
+        fetchPosts() {
+            const offset = document.getElementsByClassName('trick-item').length;
+            fetch('/load-posts/' + offset, {
+                headers: {
+                    "Content-Type": "text/html; charset=utf-8"
+                },
+            })
+                .then(data => data.text())
+                .then(result => document.getElementById('tricks').innerHTML += result)
+                .catch(error => {
+                    console.log('une erreur s\'est produite', error);
+                });
+        }
+    })
+);
+
 Alpine.start()
