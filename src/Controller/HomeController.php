401fd335deee51a6e4aeabdf9ea49749e4b307ac
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-10-08 10:55:40 +0200

    Update HomeController to get tricks from it's repository. Add new method to load more posts on the Homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
-    public function index(): Response
+    public function index(TrickRepository $trickRepository): Response
     {
+        $tricks = $trickRepository->findBy(
+            [], ['publishedDate' => 'ASC'], 3
+        );
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'tricks' => $tricks
+        ]);
+    }
+
+    public function loadMoreTricks(int $offset, TrickRepository $trickRepository): Response
+    {
+        $tricks = $trickRepository->findBy(
+            [], ['publishedDate' => 'ASC'], 5, $offset
+        );
+
+        return $this->render('tricks/tricks.html.twig', [
+            'tricks' => $tricks
         ]);
     }
 }
