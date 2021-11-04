7fa00c4fcca99a8ac53d52bca7c362f2b250abaf
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-11-04 22:12:55 +0100

    Add new method to delete one trick. Check if user has the right to do this before

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -6,6 +6,7 @@ use App\Entity\Comment;
 use App\Entity\Trick;
 use App\Form\CommentFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class TrickController extends AbstractController
@@ -19,5 +20,26 @@ class TrickController extends AbstractController
             'trick'       => $trick,
             'commentForm' => $form->createView(),
         ]);
+    }
+
+    public function delete(int $id): RedirectResponse
+    {
+        $trick = $this->getDoctrine()
+            ->getRepository(Trick::class)
+            ->find($id);
+
+        $user = $this->getUser();
+
+        if ($trick?->getUser() !== $user || !in_array('ROLE_ADMIN', $user?->getRoles(), true)) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($trick);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Le post a bien été supprimé.');
+
+        return $this->redirectToRoute('app_home');
     }
 }
