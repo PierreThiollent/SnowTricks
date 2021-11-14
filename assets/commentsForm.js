35469bd8fb665ebdc77f2b0809d739739c8862a9
Pierre Thiollent <pierre.thiollent76@gmail.com>
2021-11-14 12:03:37 +0100

    Fix error when posting a comment on a post which doesn't have comments

--- assets/commentsForm.js
+++ assets/commentsForm.js
@@ -3,13 +3,13 @@ import Alpine from 'alpinejs';
 Alpine.data('commentsForm', () => ({
     /**
      * Methode ajoutant un commentaire en ajax
-     * @param id
      * @return {Promise<void>}
+     * @param trickId
      */
-    async postComment(id) {
+    async postComment(trickId) {
         const content = new FormData(this.$el);
 
-        const response = await fetch(`/comment/new/${id}`, {
+        const response = await fetch(`/comment/new/${trickId}`, {
             body: content,
             method: 'POST'
         });
@@ -17,7 +17,14 @@ Alpine.data('commentsForm', () => ({
         const text = await response.text();
 
         if (response.ok) {
-            document.querySelector('.comment-item:last-of-type').insertAdjacentHTML('afterend', text);
+            const lastComment = document.querySelector('.comment-item:last-of-type');
+            if (lastComment) {
+                document.querySelector('.comment-item:last-of-type').insertAdjacentHTML('afterend', text);
+            } else {
+                let noComments = document.querySelector('.commentaires').querySelector('.no-comments');
+                noComments.insertAdjacentHTML('beforebegin', text);
+                noComments.remove();
+            }
         } else {
             const error = this.$el.querySelector('.error');
             error.innerText = text;
